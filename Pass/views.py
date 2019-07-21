c662cc0524f0db4d46bdba2bdd202eccc770bba0
LeftTry <lerest.go@gmail.com>
2019-07-22 00:56:44 +0300

    version 1.0

--- Pass/views.py
+++ Pass/views.py
@@ -9,3 +9,24 @@ def APISetPass(request):
         'tg': 1238860
     }
     return JsonResponse(a)
+
+def SET(request):
+    set.pass()
+    return HttpResponse('ок')
+
+
+def APIAll (request):
+    a = {
+        'name': 'Вася',
+        'surname': 'Пупкин',
+        'password': 123456,
+        'tg': 1238860
+    }
+
+    b = {
+        'name': 'Петя',
+        'surname': 'Пупкин',
+        'password': 123456,
+        'tg': 1238860
+    }
+    return JsonResponse([a, b], safe=False)
