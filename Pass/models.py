6d8acf599f27125755ef2887199d449fe793e2e9
LeftTry <lerest.go@gmail.com>
2019-07-22 12:36:08 +0300

    add migrations

--- Pass/models.py
+++ Pass/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Person:
+class Person(models.Model):
     name = models.TextField()
     surname = models.TextField()
     otchestvo = models.TextField()
